eade4b2e43bbb94758ab8480e8039a2d6a9497c7
tomdelahaba <tomdelahaba@gmail.com>
2021-11-06 23:45:33 +0100

    Firebase configuration changed

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -4,7 +4,15 @@ import "firebase/compat/auth";
 // import keys from "../config/config";
 
 // const fConfig = keys.publishableFirebaseKey;
-const fConfig = process.env.REACT_APP_FIREBASE_KEY;
+const fConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
 console.log(fConfig);
 
